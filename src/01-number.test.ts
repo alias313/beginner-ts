6845ce59ea0f691b392ab869f34acf9b20ef0b73
Elies <68759092+alias313@users.noreply.github.com>
2025-07-23 11:59:10 +0200

    Starting

---
+++ src/01-number.test.ts
@@ -0,0 +1,13 @@
+import { expect, it } from "vitest";
+
+export const addTwoNumbers = (a: number, b: number) => {
+    return a + b;
+};
+
+it("Should add the two numbers together", () => {
+    expect(addTwoNumbers(1, 2)).toBe(3);
+    expect(addTwoNumbers(-1, 1)).toBe(0);
+    expect(addTwoNumbers(0, 0)).toBe(0);
+    expect(addTwoNumbers(100, 200)).toBe(300);
+    expect(addTwoNumbers(-50, -50)).toBe(-100);
+});
