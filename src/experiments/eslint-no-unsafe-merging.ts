e8c0ecdfb82fbe7964ce9917f06c636636a0aa8f
Elies <68759092+alias313@users.noreply.github.com>
2025-07-28 15:01:40 +0200

    You will expect errors and you will be happy

--- src/experiments/eslint-no-unsafe-merging.ts
+++ src/experiments/eslint-no-unsafe-merging.ts
@@ -5,7 +5,7 @@ interface Process {
 interface Process {
 	action(): void;
 };
-
+// @ts-expect-error: Not merging all the properties of merging interfaces
 const process: Process = {
 	action: () => {},
 };
