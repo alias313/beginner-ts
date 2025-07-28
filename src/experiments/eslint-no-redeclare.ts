8e9311f344c7e3152b88974bf7a1892a238b6b95
Elies <68759092+alias313@users.noreply.github.com>
2025-07-28 15:15:31 +0200

    Catch at source

--- src/experiments/eslint-no-redeclare.ts
+++ src/experiments/eslint-no-redeclare.ts
@@ -2,9 +2,10 @@ interface Process {
 	signal: number;
 };
 
-interface Process {
+interface Process { // @typescript-eslint/no-redeclare with `ignoreDeclarationMerge: false` will catch the error at source
 	action(): void;
 };
+
 // @ts-expect-error: Not merging all the properties of merging interfaces
 const process: Process = {
 	action: () => {},
